749c6cd5c43b8f18a060281d65be76f3a5c94397
Darzho <marcosescandell97@gmail.com>
2021-10-26 10:55:06 +0200

    Entregable 3

---
+++ entregable3-marcos-escandell/Assets/Entregable 3/Script/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            transform.localScale += new Vector3(0.5f, 0, 0);
+        }
+    }
+}
